0d8f352d4f4c944574c42ecd392350ee00469963
bipinlongjam <bipinlongjam11@gmail.com>
2024-05-29 15:50:04 +0530

    add product-testtimonial-track

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,22 @@
 import React, { useContext } from 'react'
 import Layout from '../../components/Layout/Layout'
 import myContext from '../../context/data/myContext'
+import HeroSection from '../../components/heroSection/HeroSection'
+import Filter from '../../components/filter/Filter'
+import ProductCard from '../../components/productCard/ProductCard'
+import Testimonial from '../../components/testimonial/Testimonial'
+import Track from '../../components/track/Track'
 
 const Home = () => {
     const context = useContext(myContext)
     
   return (
     <Layout>
-      Home
-      
+      <HeroSection/>
+      <Filter/>
+      <ProductCard/>
+      <Track/>
+      <Testimonial/>
     </Layout>
   )
 }
